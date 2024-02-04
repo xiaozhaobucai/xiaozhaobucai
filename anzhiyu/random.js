47521910549463c34aae6481a1d41edd82b184a0
zhaowa001 <xiaowazhao0131@163.com>
2024-02-04 17:40:56 +0800

    Site updated: 2024-02-04 17:40:55

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["57449.html","24893.html","53792.html","18565.html","64764.html","64764666.html","647646565.html","64764303.html","647656.html","undefined.html","undefined.html","undefined.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
